fd6a9e72830c996fabcd3de7a3e8974d20913cfa
yang <yang@gmail.com>
2022-10-12 11:19:13 +0800

    feat: post response user table_name&id

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -41,19 +41,31 @@ class PostController extends Controller
     public function store(Request $request)
     {
 
-        // try {
-        //     $validResult = $request->validate([
-        //         "title" => "required|string",
-        //         "content" => "required|string"
-        //     ]);
-        //     $Post = Post::create($request->all());
-            
-        //     return response()->json($Post,201);
-        // } catch (ValidationException $exception) {
-        //     $errorMessage =
-        //         $exception->validator->getMessageBag()->getMessages();
-        //         return response()->json(['message'=>"incorrent format"],400);
-        // }
+        try {
+            $validResult = $request->validate([
+                "title" => "required|string",
+                "content" => "required|string",
+                'user_id' => 'required|uuid'
+            ]);
+            $Post = Post::create($request->all());
+            $User = User::where('id', $request->input('user_id'))->firstOrFail();
+            return response()->json([
+                'id'=>$Post->id,
+                'title'=>$Post->title,
+                'content'=>$Post->content,
+                'created_at'=>$Post->created_at,
+                'updated_at'=>$Post->updated_at,
+                'user'=>[
+                    'id'=>$User->id,
+                    'name'=>$User->name
+                ]
+            ],201);
+            //return response()->json($Post,201);
+        } catch (ValidationException $exception) {
+            $errorMessage =
+                $exception->validator->getMessageBag()->getMessages();
+                return response()->json(['message'=>"incorrent format"],400);
+        }
     }
 
     /**
